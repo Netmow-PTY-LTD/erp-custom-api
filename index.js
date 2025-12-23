c2ec9b83b64823d9e8e10871d6bba3a27d2aa5ee
maksudul-netmow <maksud.netmow@gmail.com>
2025-12-23 18:47:29 +0600

    23-12-25

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`HMS API listening on http://localhost:${PORT}`));
 
+
 /*
 
 require('dotenv').config();
